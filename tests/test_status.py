9f2df1b7a45bf5d1922dab85cf91ff4918674918
Mr. Senko <atodorov@mrsenko.com>
2019-04-10 16:42:03 +0300

    Switch from TestCaseRun to TestExecution API. Fixes #7
    this is compatible with Kiwi TCMS v6.7 or newer

--- tests/test_status.py
+++ tests/test_status.py
@@ -11,14 +11,14 @@ class GivenStatusCache(PluginTestCase):
         super().setUpClass()
         cls.backend._statuses = {'PASSED': 1}
         cls.backend.rpc = MagicMock()
-        cls.backend.rpc.TestCaseRunStatus.filter = MagicMock()
+        cls.backend.rpc.TestExecutionStatus.filter = MagicMock()
 
     def test_when_status_in_cache_then_return_from_cache(self):
         self.backend.get_status_id('PASSED')
-        self.backend.rpc.TestCaseRunStatus.filter.assert_not_called()
+        self.backend.rpc.TestExecutionStatus.filter.assert_not_called()
 
     def test_when_status_not_in_cache_then_return_from_cache(self):
         self.backend.get_status_id('FAILED')
-        self.backend.rpc.TestCaseRunStatus.filter.assert_called_with({
+        self.backend.rpc.TestExecutionStatus.filter.assert_called_with({
             'name': 'FAILED',
         })
