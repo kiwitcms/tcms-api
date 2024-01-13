7f440a2ca13244e85fe67ef745cf0c3a6af0fd19
Alexander Todorov <atodorov@otb.bg>
2024-01-13 13:21:18 +0200

    Refactor RPC connection refresh using a proxy-class
    the main reason here is that after a refresh we need to call .login()
    method on the underlying implementor class instead of only refreshing
    the actual transport class.
    
    Update how we use mock in tests b/c the SSL connection is now
    established just before accessing the `.rpc` attribute.

--- tests/krb5/integration_test.py
+++ tests/krb5/integration_test.py
@@ -38,30 +38,34 @@ class DoNotVerifySSLSession(requests.sessions.Session):
 class IntegrationTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        with patch("requests.sessions.Session") as session:
-            session.return_value = DoNotVerifySSLSession()
-            cls.rpc = TCMS().exec
+        cls.rpc = TCMS().exec
 
     def test_readonly_filtering_works(self):
-        results = self.rpc.Product.filter({})
-        self.assertGreater(len(results), 0)
+        with patch("requests.sessions.Session") as session:
+            session.return_value = DoNotVerifySSLSession()
+
+            results = self.rpc.Product.filter({})
+            self.assertGreater(len(results), 0)
 
     def test_create_objects_works(self):
-        now = datetime.now().isoformat()
+        with patch("requests.sessions.Session") as session:
+            session.return_value = DoNotVerifySSLSession()
 
-        result = self.rpc.Classification.filter(
-            {
-                "name": "test-products",
-            }
-        )[0]
-        self.assertEqual(result["name"], "test-products")
-        classification_id = result["id"]
+            now = datetime.now().isoformat()
 
-        product_name = "tcms-api-%s" % now
-        result = self.rpc.Product.create(
-            {"name": product_name, "classification": classification_id}
-        )
-        self.assertEqual(result["name"], product_name)
+            result = self.rpc.Classification.filter(
+                {
+                    "name": "test-products",
+                }
+            )[0]
+            self.assertEqual(result["name"], "test-products")
+            classification_id = result["id"]
+
+            product_name = "tcms-api-%s" % now
+            result = self.rpc.Product.create(
+                {"name": product_name, "classification": classification_id}
+            )
+            self.assertEqual(result["name"], product_name)
 
 
 if __name__ == "__main__":
