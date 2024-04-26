7b0ce0e457bd12eb7bf46b1e8a8f1136aa6ffefc
Alexander Todorov <atodorov@otb.bg>
2024-04-26 20:15:28 +0300

    Add integration test for passsing credentials via Python code

--- tests/krb5/kiwitcms_kerberos/db_init.py
+++ tests/krb5/kiwitcms_kerberos/db_init.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
 
 from tcms.management.models import Classification
 from tcms.utils.permissions import initiate_user_with_default_setups
@@ -16,3 +17,17 @@ initiate_user_with_default_setups(bot)
 
 # this is used inside integration test
 Classification.objects.create(name="test-products")
+
+# account only to verify credentials passed
+# via Python source code, not config file
+developer = User.objects.create(
+    username="kiwitcms-developer",
+    email="developler@example.com",
+    is_active=True,
+)
+developer.set_password("hack-me")
+developer.save()
+initiate_user_with_default_setups(developer)
+developer.user_permissions.add(
+    Permission.objects.get(content_type__app_label="auth", codename="view_user")
+)
